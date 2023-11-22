1e834d10b1448469f07ccd6d51d93c94dc5b6327
Jhansi <jhansisridevi@gmail.com>
2023-11-22 16:49:47 -0500

    starter index file

--- index.mjs
+++ index.mjs
@@ -1,11 +1,16 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import routes from './routes/userRoute.mjs'
+import bodyParser from 'body-parser';
 
 dotenv.config();
 const PORT = process.env.PORT || 3500;
 const app = express();
 
+app.use(bodyParser.json());
 app.use(express.json())
+
+app.use("/",routes); //routes
 
 app.listen(PORT, () =>{
     console.log(`Server on port: ${PORT}`);
